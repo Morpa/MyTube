52d77dc960f60b3f0c5e2ed4d5cc7a21800b6aa5
Morpa <andremorpa@gmail.com>
2019-01-02 11:29:15 +0000

    Busca video ok

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Segment, Input } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { buscaVideo } from '../store/actions/busca-video'
 
 class SearchBar extends Component {
 
     pesquisaTermo = e => {
-        if(e.keyCode === 13) {
+        if (e.keyCode === 13) {
             const termo = e.target.value
             console.log(termo)
+            this.props.buscaVideo(termo)
         }
     }
 
@@ -14,11 +17,17 @@ class SearchBar extends Component {
         return (
             <div className='search-bar'>
                 <Segment stacked>
-                    <Input icon='search' onKeyDown={ (e) => this.pesquisaTermo(e) } size='large' placeholder='Search...' />
+                    <Input icon='search' onKeyDown={(e) => this.pesquisaTermo(e)} size='large' placeholder='Search...' />
                 </Segment>
             </div>
         )
     }
 }
 
-export default SearchBar
+const mapDispatchToProps = (dispatch) => {
+    return {
+        buscaVideo: (termo) => dispatch(buscaVideo(termo))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar)
